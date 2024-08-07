6abe9eb8c04a2a2e6abd9b6512f9e1417bb6e9b9
rsgm1978 <159719756+rsgm1978@users.noreply.github.com>
2024-08-07 16:09:51 +0000

    acabado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let textForMap = ' '
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  pronoun.map((elementPronuon) => {
+    adj.map((elementAdj) => {
+      noun.map((elementNoun) => {
+        textForMap += `<li class="list-group-item">
+            ${elementPronuon}${elementAdj}${elementNoun}.es
+            </li>`
+          
+    })
+  })
+})
+  
+  document.querySelector("#for-map").innerHTML = textForMap
+  
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
 };
